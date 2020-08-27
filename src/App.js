7f8242877fea28e9666bd1638c2f68a0d0718407
nstammewar <nstammewar@gmail.com>
2020-08-27 16:22:54 +0530

    added hot module refresh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to React';
-    return
-    (
-      <div className="App">
-        <h2>{helloWorld}</h2>
-      </div>
+    const helloWorld = 'Welcome to React';
+    return(
+      <div className="App"> <h2>{helloWorld}</h2> </div> 
     );
   }
 }
